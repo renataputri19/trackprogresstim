79d30ab1ea495da7a0f78ee7c5e0e98417389713
renataputri19 <renataputri19@gmail.com>
2024-06-11 11:01:09 +0700

    testing

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,19 +2,25 @@
 
 namespace App\Providers;
 
-use App\Models\Task;
-use App\Policies\TaskPolicy;
+// use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
+    /**
+     * The model to policy mappings for the application.
+     *
+     * @var array<class-string, class-string>
+     */
     protected $policies = [
-        Task::class => TaskPolicy::class,
+        //
     ];
 
-    public function boot()
+    /**
+     * Register any authentication / authorization services.
+     */
+    public function boot(): void
     {
-        $this->registerPolicies();
+        //
     }
 }
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -8,8 +8,6 @@
     <title>@yield('title', 'Business Tracker')</title>
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <!-- Include FullCalendar CSS and JS -->
-    <link href="{{ mix('css/fullcalendar.css') }}" rel="stylesheet">
 </head>
 <body>
     <header class="bg-white shadow">
@@ -36,6 +34,5 @@
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <script src="{{ mix('js/fullcalendar.js') }}"></script>
 </body>
 </html>
