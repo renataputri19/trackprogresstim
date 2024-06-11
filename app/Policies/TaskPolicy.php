e617743513a4b17e1238724cbc60f7ab777bc9e8
Renata Putri Henessa <Renataputri19@gmail.com>
2024-06-11 15:08:46 +0700

    Removed SSH private key

---
+++ app/Policies/TaskPolicy.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Task;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class TaskPolicy
+{
+    use HandlesAuthorization;
+
+    // Determine if the given task can be updated by the user
+    public function update(User $user, Task $task)
+    {
+        return $user->id === $task->user_id;
+    }
+
+    // Determine if the given task can be deleted by the user
+    public function delete(User $user, Task $task)
+    {
+        return $user->id === $task->user_id;
+    }
+}
