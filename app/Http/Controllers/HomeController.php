dd1f3e1a8f3460b346c0cd641b35b69647ff0b71
Renata Putri Henessa <Renataputri19@gmail.com>
2024-07-01 15:55:46 +0700

    testing dev

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,17 +7,7 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
-    /**
-     * Show the application dashboard.
+     * Show the application homepage.
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
@@ -26,3 +16,4 @@ class HomeController extends Controller
         return view('home');
     }
 }
+
