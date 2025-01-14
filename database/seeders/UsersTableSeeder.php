f16eeb83745ff9c8b5a3e3bfb741c66af15a633c
Renata Putri Henessa <Renataputri19@gmail.com>
2025-01-14 13:14:43 +0700

    database adding

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -10,6 +10,13 @@ class UsersTableSeeder extends Seeder
 {
     public function run()
     {
-        
+
+$users = [
+            ['name' => 'Nimta Nababan, S.Mat.', 'email' => 'nimta@bps.go.id', 'password' => Hash::make('340054831'), 'is_admin' => 1],
+            ['name' => 'Sri Fahrina, A.Md.Stat', 'email' => 'sri.fahrina@bps.go.id', 'password' => Hash::make('340060430'), 'is_admin' => 1],
+            ['name' => 'Florentz Magdalena', 'email' => 'fmagdalena@bps.go.id', 'password' => Hash::make('340056837'), 'is_admin' => 1],
+        ];
+
+        DB::table('users')->insert($users);
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -90,12 +90,12 @@
                         {{-- <a href="https://rb.bpsbatam.com/" class="app-item">
                             <div class="app-icon"><i class="fas fa-balance-scale"></i></div>
                             <div class="app-title">Reformasi Birokrasi</div>
-                        </a>
+                        </a> --}}
                         <!-- New Link for Generate Text feature -->
                         <a href="{{ route('generate.form') }}" class="app-item">
                             <div class="app-icon"><i class="fas fa-file-alt"></i></div>
                             <div class="app-title">Generate Text</div>
-                        </a> --}}
+                        </a>
                         <!-- New Link for Padamu Negri -->
                         <a href="{{ url('/padamunegri') }}" class="app-item">
                             <div class="app-icon"><i class="fas fa-university"></i></div>
