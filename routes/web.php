ae19bcbd6bd52af59d47045ce0334ec587666695
Renata Putri Henessa <Renataputri19@gmail.com>
2026-01-13 09:20:00 +0700

    sbr -9

--- routes/web.php
+++ routes/web.php
@@ -48,7 +48,9 @@ Route::get('/sbr', [App\Http\Controllers\SbrController::class, 'index'])->name('
 Route::get('/sbr/search', [App\Http\Controllers\SbrController::class, 'search'])->name('sbr.search');
 Route::get('/sbr/stats', [App\Http\Controllers\SbrController::class, 'stats'])->name('sbr.stats');
 Route::get('/sbr/kelurahan/{kecamatan}', [App\Http\Controllers\SbrController::class, 'getKelurahan'])->name('sbr.kelurahan');
-Route::get('/sbr/{id}', [App\Http\Controllers\SbrController::class, 'show'])->name('sbr.show');
+Route::get('/sbr/{id}', [App\Http\Controllers\SbrController::class, 'show'])
+    ->whereNumber('id')
+    ->name('sbr.show');
 Route::put('/sbr/{id}', [App\Http\Controllers\SbrController::class, 'update'])->name('sbr.update');
 Route::delete('/sbr/{id}/tagging', [App\Http\Controllers\SbrController::class, 'clearTagging'])->name('sbr.clear');
 
